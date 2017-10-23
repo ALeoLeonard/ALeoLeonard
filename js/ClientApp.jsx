e33a3b516dc8ae1b4bf60af8de2ff6df05580a4f
Andrew Leonard <ALeoLeonard@gmail.com>
2017-10-23 00:01:59 -0400

    Change to Browser Router update git ignore

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Landing from './Landing'
 
 const App = () => (
-  <HashRouter>
-    <div className='app'>
-      <Route exact path='/' component={Landing} />
-    </div>
-  </HashRouter>
+  <BrowserRouter>
+    <Switch>
+      <div className='app'>
+        <Route exact path='/' component={Landing} />
+      </div>
+    </Switch>
+  </BrowserRouter>
 )
 
 render(<App />, document.getElementById('app'))
